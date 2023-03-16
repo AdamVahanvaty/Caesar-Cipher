67415672016372ad3629b816c5be51c929bea692
AdamVahanvaty <Adam.Vahanvaty@gmail.com>
2023-03-16 23:58:19 +1100

    First Commit

---
+++ Caesar-Cipher.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string plaintext;
+    int shift;
+    std::cout << "Enter Plaintext: ";
+    std::getline(std::cin, plaintext);
+    std::cout << "Enter the shift value: ";
+    std::cin >> shift;
+    std::string ciphertext = encrypt(plaintext, shift);
+    std::cout << "Ciphertext: " << ciphertext << "\n";
+    return 0;
+}
+
+// Function to encrypt plaintext using Caesar Cipher
+std::string encrypt(std::string plaintext, int shift) {
+    std::string ciphertext = "";
+    for (int i = 0; i < plaintext.length(); i++) {
+        // Encrypt uppercase characters
+        if (std::isupper(plaintext[i])) {
+            ciphertext += char(int(plaintext[i] + shift - 65) % 26 + 65);
+        }
+        // Encrypt lowercase characters
+        else if (std::islower(plaintext[i])) {
+            ciphertext += char(int(plaintext[i] + shift - 97) % 26 + 97);
+        }
+        // Ignore other characters (e.g. spaces)
+        else {
+            ciphertext += plaintext[i];
+        }
+    }
+    return ciphertext;
+}
